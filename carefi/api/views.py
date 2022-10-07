3a0222b753315b71f7accb9c26c37cbd1377b01f
jemesh <jemeshkhatik7000@gmail.com>
2022-10-07 15:10:24 +0530

    removed extra comments

--- carefi/api/views.py
+++ carefi/api/views.py
@@ -8,26 +8,6 @@ from .serializers import priceofbitcoinserializer
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
-"""
-def add(request):
-    # getting the request from url
-    url = "https://www.google.com/search?q=bitcoin+price"
-    data = requests.get(url)
-    # converting the text
-    soup = BS(data.text, 'html.parser')
-    # finding meta info for the current price
-    ans = soup.find("div", class_="BNeawe iBp4i AP7Wnd").text
-    # returning the price
-    user=priceofbitcoin.objects.create(price=ans)
-    user.save()
-    form=user
-    return render(request,'add.html',{'form':form})
-
-class priceofbitcoinviewset(viewsets.ModelViewSet):
-    queryset = priceofbitcoin.objects.all()
-    serializer_class = priceofbitcoinserializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-"""
 class priceofbitcoinviewset(viewsets.ViewSet):
     def data(self):
         url = "https://www.google.com/search?q=bitcoin+price"
